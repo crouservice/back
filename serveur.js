d07fefa101e27bfdb316a654242a37098e32f289
Braisetheghost <123772211+Braisetheghost@users.noreply.github.com>
2024-03-19 15:34:39 +0100

    ajout geoloc

--- serveur.js
+++ serveur.js
@@ -10,6 +10,10 @@ var request =require("request");
 
 var url ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?limit=20"
 
+
+
+
+
 request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res,data)=>{
     if(err){
         console.log("Error:",err);
@@ -19,6 +23,8 @@ request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type
     }
 })
 
+
+
 var url2="/liste"
 app.get(url2,(req,res)=>{
     res.json(
@@ -27,6 +33,33 @@ app.get(url2,(req,res)=>{
 })
 
 
+app.get("/localisation/:lat/:lon", (req, res) => {
+    var lat=parseInt(req.params.lat);
+    var lon=parseInt(req.params.lon);
+    let coord=encodeURIComponent("POLYGON (("+(lon-1)+" "+ (lat+5)+"," +(lon-1)+" "+ (lat-5)+","+(lon+1)+" "+ (lat+5)+","+(lon+1)+" "+ (lat-5)+","+(lon-1)+" "+ (lat+5)+"))")
+    
+    console.log(coord);
+    var tmp="intersects(geocalisation%2C%20geom%27"+coord+"%27)" ;
+    var urlFinal ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?where="+tmp+"&limit=20";
+    url=urlFinal;
+    request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
+        if(err){
+            console.log("Error:",err);
+        }else {
+            res.json(
+                data["results"]
+            )
+        }
+    })
+})
+
+
+
 app.listen(port,()=>{
     console.log('Server is running on port 3080');
 })
+
+
+
+
+
