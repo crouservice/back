f03a14577fb67924f9a4a2a7e86890cd1f3db8e5
Braisetheghost <123772211+Braisetheghost@users.noreply.github.com>
2024-03-21 13:57:12 +0100

    ajout autre api

--- serveur.js
+++ serveur.js
@@ -8,40 +8,14 @@ app.use(cors());
 
 var request =require("request");
 
-var url ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?limit=20"
-
-
-
-
-
-request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res,data)=>{
-    if(err){
-        console.log("Error:",err);
-    }else {
-        //console.log(data);
-        donne=JSON.parse(JSON.stringify(data));
-    }
-})
-
-
-
-var url2="/liste"
-app.get(url2,(req,res)=>{
-    res.json(
-        donne["results"]
-    )
-})
-
-
-app.get("/localisation/:lat/:lon", (req, res) => {
+app.get("/logement/:lat/:lon", (req, res) => {
     var lat=parseInt(req.params.lat);
     var lon=parseInt(req.params.lon);
     let coord=encodeURIComponent("POLYGON (("+(lon-1)+" "+ (lat+5)+"," +(lon-1)+" "+ (lat-5)+","+(lon+1)+" "+ (lat+5)+","+(lon+1)+" "+ (lat-5)+","+(lon-1)+" "+ (lat+5)+"))")
-    
+
     console.log(coord);
     var tmp="intersects(geocalisation%2C%20geom%27"+coord+"%27)" ;
-    var urlFinal ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?where="+tmp+"&limit=20";
-    url=urlFinal;
+    var url="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?where="+tmp+"&limit=20";
     request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
         if(err){
             console.log("Error:",err);
@@ -53,13 +27,42 @@ app.get("/localisation/:lat/:lon", (req, res) => {
     })
 })
 
+app.get("/restaurant/:lat/:lon", (req, res) => {
+    var lat=parseInt(req.params.lat);
+    var lon=parseInt(req.params.lon);
+    let coord=encodeURIComponent("POLYGON (("+(lon-1)+" "+ (lat+5)+"," +(lon-1)+" "+ (lat-5)+","+(lon+1)+" "+ (lat+5)+","+(lon+1)+" "+ (lat-5)+","+(lon-1)+" "+ (lat+5)+"))")
 
+    console.log(coord);
+    var tmp="intersects(geolocalisation%2C%20geom%27"+coord+"%27)" ;
+    var url ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_restauration_france_entiere/records?where="+tmp+"&limit=20";
+
+    console.log(decodeURIComponent(url))
+    request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
+        if(err){
+            console.log("Error:",err);
+        }else {
+            res.json(
+                data["results"]
+            )
+        }
+        console.log(data)
+    })
+})
+
+app.get("/etablissement", (req, res) => {
+    var url ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-principaux-etablissements-enseignement-superieur/records?limit=100&refine=type_d_etablissement%3A%22Universit%C3%A9%22&refine=type_d_etablissement%3A%22Grand%20%C3%A9tablissement%22&refine=type_d_etablissement%3A%22Autre%20%C3%A9tablissement%22";
+
+    request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
+        if(err){
+            console.log("Error:",err);
+        }else {
+            res.json(
+                data["results"]
+            )
+        }
+    })
+})
 
 app.listen(port,()=>{
     console.log('Server is running on port 3080');
 })
-
-
-
-
-
