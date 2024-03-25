fefa2e1f66e9a76f3a4220c77950583355e5a829
Braisetheghost <123772211+Braisetheghost@users.noreply.github.com>
2024-03-25 22:07:39 +0100

    changement filtre de recherche

--- serveur.js
+++ serveur.js
@@ -8,11 +8,12 @@ app.use(cors());
 
 let request =require("request");
 
+types_exclu=[""];
 
-types_recherche_rest = ["cafétéria", "restaurant", "brasserie", "foodtruck", "kiosque", "libre-service", "coffee corner", "épicerie", "triporteur", "sandwicherie", "crous and go"];
+types_recherche_rest = ["cafétéria", "restaurant", "brasserie", "foodtruck", "kiosque", "libre-service", "coffee corner", "épicerie", "triporteur", "sandwicherie", "crous and go","cafet"];
 zones_recherche_log = ["amiens", "angers", "arras", "avignon", "auxerre", "besançon", "bobigny", "bordeaux", "bourges", "brest", "caen", "cachan", "chartres", "chambéry", "clermont-ferrand", "compiegne", "creteil", "dijon", "grenoble", "guadeloupe", "hauts-de-seine", "herouville-saint-clair", "garde", "rochelle", "landes", "bourget", "havre", "cezeaux", "limoges", "lille", "lyon", "marseille", "martinique", "metz", "montpellier", "mont-saint-aignan", "mulhouse", "nancy", "nantes", "nice", "nîmes", "orléans", "pau", "perpignan", "poitiers", "reims", "rennes", "roubaix", "rouen", "saint-étienne", "sénart", "strasbourg", "talence", "toulouse", "tours", "d'oise", "paris", "valence", "vandœuvre-lès-nancy", "villeurbanne", "villeneuve", "yvelines","mans"];
 
-commun_recherche=["pmr", "parking", "laverie", "cuisine", "douche", "kitchenette", "internet", "garage", "securite", "restauration", "travail"];
+commun_recherche=["pmr", "parking", "laverie", "cuisine", "douche", "kitchenette", "internet", "garage", "securite", "restauration", "travail","chambre","logement","résidence"];
 
 
 app.get("/trie", (req, res) => {
@@ -36,7 +37,7 @@ function lien_rech(tab, types,exclus,type,infos) {
   if(tab[0]=="" && tab.length>1 ){
     tab.shift();
   }
-  console.log(tab)
+  //console.log(tab)
   let deb=0
   for(deb ;deb<tab.length && estType(tab[deb],exclus);deb++);
   //if(estType(tab[deb], exclus))return "where="+encodeURIComponent('search('+type+',azerty)');
@@ -47,7 +48,7 @@ function lien_rech(tab, types,exclus,type,infos) {
   if (tab.length > 1) {
     for (i = deb+1; i < tab.length; i++) {
       if (estType(tab[i], types))
-        res = res + ' or search('+type+',"' + tab[i] + '")';
+        res = 'search('+type+',"' + tab[i] + '")'+'and '+res;
       else if (!estType(tab[i], exclus))res = res + ' and  search('+infos+',"' + tab[i] + '")';
     }
   }
@@ -62,14 +63,14 @@ function lien_rech(tab, types,exclus,type,infos) {
 app.get("/restaurant/:data", (req, res) => {
   let Jtrie = JSON.parse(req.params.data);
 
-  let trie = lien_rech(Jtrie.trie, types_recherche_rest,zones_recherche_log,"type","infos");
-  console.log("trie",trie)
+  let trie = lien_rech(Jtrie.trie,zones_recherche_log,types_exclu,"zone","infos");
+  //console.log("trie",trie)
   let url =
        "https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_restauration_france_entiere/records?" +
        trie +
        "&limit=10";
   
-  console.log("url",url)
+  //console.log("url",url)
   request.get(
     {
     url: url,
@@ -78,7 +79,7 @@ app.get("/restaurant/:data", (req, res) => {
     },
     (err, res1, data) => {
     if (err) {
-        console.log("Error:", err);
+        //console.log("Error:", err);
     } else {
         res.json(data["results"]);
       }
@@ -89,14 +90,14 @@ app.get("/restaurant/:data", (req, res) => {
 app.get("/logement/:data", (req, res) => {
   let Jtrie = JSON.parse(req.params.data);
 
-  let trie =lien_rech(Jtrie.trie, zones_recherche_log,types_recherche_rest,"zone","infos");
-  console.log("trie",trie)
+  let trie =lien_rech(Jtrie.trie, zones_recherche_log,types_exclu,"zone","infos");
+  //console.log("trie",trie)
   let url =
        "https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?"
        +
        trie
        +"&limit=10";
-  console.log("url",url)
+  //console.log("url",url)
   request.get({
       url: url,
       json: true,
@@ -104,7 +105,7 @@ app.get("/logement/:data", (req, res) => {
   },
   (err, res1, data) => {
       if (err) {
-        console.log("Error:", err);
+        //console.log("Error:", err);
       } else {
           res.json(data["results"]);
         }
@@ -125,7 +126,7 @@ app.get("/logement/:latHG/:lonHG/:latBD/:lonBD", (req, res) => {
     let url="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_logement_france_entiere/records?where="+tmp+"&limit=100";
     request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
         if(err){
-            console.log("Error:",err);
+            //console.log("Error:",err);
         }else {
             res.json(
                 data["results"]
@@ -146,10 +147,10 @@ app.get("/restaurant/:latHG/:lonHG/:latBD/:lonBD", (req, res) => {
     let tmp="intersects(geolocalisation%2C%20geom%27"+coord+"%27)" ;
     let url ="https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr_crous_restauration_france_entiere/records?where="+tmp+"&limit=100";
 
-    console.log(decodeURIComponent(url))
+    //console.log(decodeURIComponent(url))
     request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
         if(err){
-            console.log("Error:",err);
+            //console.log("Error:",err);
         }else {
             res.json(
                 data["results"]
@@ -164,7 +165,7 @@ app.get("/etablissement", (req, res) => {
 
     request.get({url: url,json :true ,headers:{"User-Agent" :"request","Content-Type" :"application/json"}},(err,res1,data)=>{
         if(err){
-            console.log("Error:",err);
+            //console.log("Error:",err);
         }else {
             res.json(
                 data["results"]
